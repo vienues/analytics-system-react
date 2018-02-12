968197a6379526a0b0c724feb8bab08defb5c631
vienues <mjohnlynnc@gmail.com>
2018-02-12 20:36:54 +0000

    add price subscription

---
+++ pricing/index.ts
@@ -0,0 +1,49 @@
+import redis from 'redis';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+
+const pub = redis.createClient({host: 'localhost', port: 6379});
+const sub = redis.createClient({host: 'localhost', port: 6379});
+
+const randomPrice = () => (Math.random() * (max - min) + min).toFixed(2);
+
+const max = 66;
+const min = 62;
+
+function createPriceStream(symbol: string) {
+  return Observable.interval(1000).map(_ => JSON.stringify({
+    change: randomPrice(),
+    id: symbol,
+    volume: randomPrice(),
+  }));
+}
+
+const subscriptions = new Map();
+const SUBSCRIBE_TO_INDEX_UPDATES = 'SUBSCRIBE_TO_INDEX_UPDATES';
+
+interface IHandlers {
+  [channel: string]: (message: string) => void;
+}
+
+const createTopic = (symbol: string) => `INDEX_UPDATE.${symbol}`;
+
+const handlers: IHandlers = {
+  [SUBSCRIBE_TO_INDEX_UPDATES]: (message: string) => {
+
+    const symbols: string[] = JSON.parse(message);
+
+    symbols.filter(symbol => !subscriptions.has(symbol))
+      .forEach((symbol: string) => {
+        const subscription = createPriceStream(symbol).subscribe((price) => pub.publish(createTopic(symbol), price));
+        subscriptions.set(symbol, subscription);
+      });
+  }
+};
+
+sub.on('message', (channel: string, message: string) => {
+  handlers[channel].call(null, message);
+});
+
+
+sub.subscribe('SUBSCRIBE_TO_INDEX_UPDATES');
--- server/resolvers/index.js
+++ server/resolvers/index.js
@@ -1,32 +1,9 @@
 import fetch from 'node-fetch';
-import { PubSub } from 'graphql-subscriptions';
-import * as Ix from 'ix';
-
 import date from './date';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 
 const ENDPOINT = 'https://api.iextrading.com/1.0';
-const max = 66;
-const min = 62;
-
-async function* gen() {
-  while (true) {
-    yield new Promise((resolve) => {
-      setInterval(() => {
-        resolve({
-          indices: [{
-            id: 'DOWJONES',
-            name: "Dow Jones",
-            change: randomPrice(),
-            volume: randomPrice(),
-          }]
-        })
-      }, 1000)
-    })
-  }
-}
-
-
-const randomPrice = () => (Math.random() * (max - min) + min).toFixed(2)
+const pubsub = new RedisPubSub();
 
 async function fetchAndParse(url) {
   return (await fetch(url)).json();
@@ -35,12 +12,19 @@ async function fetchAndParse(url) {
 export default {
   ...date,
   Subscription: {
-    indices: {
-      subscribe: () => Ix.AsyncIterable.from(gen())
-    }
+    indexUpdates: {
+      resolve: (payload, args, context, info) => {
+        console.log(payload);
+        return payload;
+      },
+      subscribe: (_, args) => {
+        pubsub.publish('SUBSCRIBE_TO_INDEX_UPDATES', args.symbols);
+        return pubsub.asyncIterator(args.symbols.map(symbol => `INDEX_UPDATE.${symbol}`));
+      },
+    },
   },
   Query: {
-    async stock(_, {symbol}) {
+    async stock(_, { symbol }) {
       return {
         ...(await fetchAndParse(`${ENDPOINT}/stock/${symbol}/stats`)),
         symbol,
@@ -50,10 +34,10 @@ export default {
   },
   Stock: {
     id: stock => stock.symbol,
-    peers: ({symbol}) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/peers/`),
-    price: ({symbol}) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/price/`),
-    quote: ({symbol}) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/quote/`),
-    relatedNews: ({symbol}) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/news/`),
+    peers: ({ symbol }) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/peers/`),
+    price: ({ symbol }) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/price/`),
+    quote: ({ symbol }) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/quote/`),
+    relatedNews: ({ symbol }) => fetchAndParse(`${ENDPOINT}/stock/${symbol}/news/`),
   },
   News: {
     id: article => article.url,
