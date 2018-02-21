f380c83c1cbcc037e7302803de183c04a89d879b
vienues <mjohnlynnc@gmail.com>
2018-02-21 17:02:53 +0000

    fragment pattern

--- client/src/containers/News/connector.js
+++ client/src/containers/News/connector.js
@@ -1,22 +1,22 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import News from './News';
 
-export const StockNewsConnector = graphql(
-  gql`
-    query StockNewsConnectorQuery($id: ID!) {
-      stock(id: $id) {
-        id
-        ...News
+export const StockNewsConnector = fragment =>
+  graphql(
+    gql`
+      query StockNewsConnectorQuery($id: ID!) {
+        stock(id: $id) {
+          id
+          ...News
+        }
       }
-    }
-    ${News.fragment}
-  `,
-  {
-    options: ({ id }) => ({
-      variables: { id },
-    }),
-  },
-);
+      ${fragment}
+    `,
+    {
+      options: ({ id }) => ({
+        variables: { id },
+      }),
+    },
+  );
 
 export default StockNewsConnector;
