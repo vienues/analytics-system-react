73620e744f24050741bdd8d71ae384a9467feaa5
vienues <mjohnlynnc@gmail.com>
2018-03-09 10:36:56 -0500

    Bug fix for checking an undefined environment variable

--- server/connectors/index.js
+++ server/connectors/index.js
@@ -2,5 +2,5 @@ import * as real from './iex'
 import * as fake from './faker'
 
 export default function getDataSource(offlineMode) {
-  return offlineMode.toLowerCase() === 'true' ? fake : real
+  return typeof offlineMode === 'string' && offlineMode.toLowerCase() === 'true' ? fake : real
 }
