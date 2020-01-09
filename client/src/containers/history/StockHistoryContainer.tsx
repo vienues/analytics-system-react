a8a49b0f8ac08db2fcb104f8acb9bafb3089d470
vienues <mjohnlynnc@gmail.com>
2020-01-09 17:28:55 +0100

    fix(client): fixes top navigation not showing on graph pop-up (ARTP-917)

--- client/src/containers/history/StockHistoryContainer.tsx
+++ client/src/containers/history/StockHistoryContainer.tsx
@@ -1,8 +1,9 @@
+import React from 'react'
+
 import { AppQuery } from 'common/AppQuery'
 import { IApolloContainerProps } from 'common/IApolloContainerProps'
-import React from 'react'
 import { StockHistoryQuery, StockHistoryQueryVariables } from '../../__generated__/types'
-import { StockHistoryChart } from './compnents/StockHistoryChart'
+import { StockHistoryChart } from './components/StockHistoryChart'
 import StockHistoryConnection from './graphql/StockHistoryConnection.graphql'
 
 export const StockHistoryContainer: React.FC<IApolloContainerProps> = ({ id }) => {
@@ -14,3 +15,5 @@ export const StockHistoryContainer: React.FC<IApolloContainerProps> = ({ id }) =
     </>
   )
 }
+
+export default StockHistoryContainer
