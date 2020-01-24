87a057a447cf5293e8a3294501fca9bd50fa1f70
vienues <mjohnlynnc@gmail.com>
2020-01-24 09:32:06 +0000

    chore(dx): no need to define fin global now :-)

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,13 +1,9 @@
 const path = require('path')
 const fs = require('fs')
-const { babelInclude, override, useEslintRc } = require('customize-cra')
+const { babelInclude, override } = require('customize-cra')
 const rewireGqlTag = require('react-app-rewire-graphql-tag')
 
 const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
-module.exports = override(
-  rewireGqlTag,
-  useEslintRc(resolveApp('../.eslintrc')),
-  babelInclude([resolveApp('src'), resolveApp('../common')]),
-)
+module.exports = override(rewireGqlTag, babelInclude([resolveApp('src'), resolveApp('../common')]))
