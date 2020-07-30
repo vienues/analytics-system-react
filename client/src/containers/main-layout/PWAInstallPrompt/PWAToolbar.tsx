93a66ee48859e3fd5a673767aec645d2a27da787
vienues <mjohnlynnc@gmail.com>
2020-07-30 15:36:56 +0100

    fix(pwa): use updated toolbar colour (ARTP-1257)

--- client/src/containers/main-layout/PWAInstallPrompt/PWAToolbar.tsx
+++ client/src/containers/main-layout/PWAInstallPrompt/PWAToolbar.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useTheme, ThemeName } from 'rt-theme'
 
-const getBarColor = (color: ThemeName) => (color === ThemeName.Dark ? '#4D4D4D' : '#F1F2F2')
+const getBarColor = (color: ThemeName) => (color === ThemeName.Dark ? '#4D4D4D' : '#F9FAFA')
 
 export const PWAToolbar: React.FC = () => {
   const { themeName } = useTheme()
